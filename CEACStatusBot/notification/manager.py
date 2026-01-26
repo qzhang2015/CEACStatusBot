a874fbe19f864b106171b8daeda0396e7f4e6259
qzhang2015 <5n9qtyhpxg@privaterelay.appleid.com>
2026-01-26 19:51:45 +0800

    Update manager.py

--- CEACStatusBot/notification/manager.py
+++ CEACStatusBot/notification/manager.py
@@ -56,13 +56,13 @@ class NotificationManager:
         print(f"Current status: {current_status} - Last updated: {current_last_updated}")
         # Load the previous statuses from the file
         statuses = self.__load_statuses()
-
+        self.__send_notifications(res)
         # Check if the current status is different from the last recorded status
-        if not statuses or current_status != statuses[-1].get("status", None) or current_last_updated != statuses[-1].get("last_updated", None):
-            self.__save_current_status(current_status, current_last_updated)
-            self.__send_notifications(res)
-        else:
-            print("Status unchanged. No notification sent.")
+        # if not statuses or current_status != statuses[-1].get("status", None) or current_last_updated != statuses[-1].get("last_updated", None):
+        #     self.__save_current_status(current_status, current_last_updated)
+        #     self.__send_notifications(res)
+        # else:
+        #     print("Status unchanged. No notification sent.")
 
     def __load_statuses(self) -> list:
         if os.path.exists(self.__status_file):
